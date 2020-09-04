f259618990b162fb39e3f3e1b7b67c3116ae20e2
euniceyoh <euniceohsj@gmail.com>
2020-09-03 21:34:32 -0700

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Eunice Oh on 9/3/20.
+//  Copyright © 2020 Eunice Oh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
