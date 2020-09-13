2ec63aeec072d4fd8441005b150a613e41915632
euniceyoh <euniceohsj@gmail.com>
2020-09-13 00:05:49 -0700

    Part 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Eunice Oh on 9/12/20.
+//  Copyright © 2020 Eunice Oh. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
